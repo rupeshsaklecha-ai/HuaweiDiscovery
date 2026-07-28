ec0804f1ea2ee27ed4f471b33d946c855e7573ec
rupeshsaklecha-ai <rupeshsaklecha@gmail.com>
2026-07-28 14:58:40 +0530

    Update discover.py

--- discover.py
+++ discover.py
@@ -1,20 +1,33 @@
 from config import SWITCHES
 from snmp import *
-from oids import IFNAME
+from oids import *
 
 def discover():
 
     for sw in SWITCHES:
 
-        print(f"\nConnecting : {sw['ip']}")
+        print("="*60)
+        print("Switch :",sw["name"])
+        print("IP     :",sw["ip"])
+        print("="*60)
 
-        ports = snmp_walk(
+        ports=snmp_walk(
             sw["ip"],
             sw["community"],
             IFNAME
         )
 
-        if len(ports) == 0:
+        if len(ports)==0:
             print("SNMP Failed")
-        else:
-            print(f"SNMP OK ({len(ports)} interfaces found)")
+            continue
+
+        print("SNMP Connected")
+        print()
+
+        for oid,value in ports:
+
+            if "XGigabitEthernet" in value:
+
+                idx=oid.split(".")[-1]
+
+                print(value," ifIndex =",idx)
