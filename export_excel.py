587f583f8706d85442da53de28f82745660b6d6e
rupeshsaklecha-ai <rupeshsaklecha@gmail.com>
2026-07-28 17:18:02 +0530

    Update export_excel.py

--- export_excel.py
+++ export_excel.py
@@ -1,26 +1,42 @@
 from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
 
-
-def export_excel(ip, data):
+def export_excel(ip, excel_data):
 
     wb = Workbook()
-
     ws = wb.active
+    ws.title = "Huawei Discovery"
 
-    ws.title = "Discovery"
+    # ===== Report Header =====
+    ws.merge_cells("A1:C1")
+    ws["A1"] = "Huawei Discovery Tool V2.1"
+    ws["A1"].font = Font(size=16, bold=True)
+    ws["A1"].alignment = Alignment(horizontal="center")
 
-    ws.append([
-        "Port Name",
-        "ifIndex",
-        "entPhysicalIndex"
-    ])
+    ws.merge_cells("A2:C2")
+    ws["A2"] = "Developed by Sun Technologies"
+    ws["A2"].font = Font(size=12, bold=True)
+    ws["A2"].alignment = Alignment(horizontal="center")
 
-    for row in data:
+    # ===== Switch Info =====
+    ws["A4"] = "Switch IP"
+    ws["B4"] = ip
 
-        ws.append(row)
+    # ===== Table Header =====
+    ws["A6"] = "Port Name"
+    ws["B6"] = "ifIndex"
+    ws["C6"] = "entPhysicalIndex"
 
-    filename = f"HuaweiDiscovery_{ip}.xlsx"
+    for cell in ["A6", "B6", "C6"]:
+        ws[cell].font = Font(bold=True)
 
-    wb.save(filename)
+    # ===== Data =====
+    row = 7
 
-    print("Saved :", filename)
+    for data in excel_data:
+        ws.cell(row=row, column=1).value = data[0]
+        ws.cell(row=row, column=2).value = data[1]
+        ws.cell(row=row, column=3).value = data[2]
+        row += 1
+
+    wb.save(f"HuaweiDiscovery_{ip}.xlsx")
