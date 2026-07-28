d7f9cee5a3d6150b048f2ae19d8e62d62365d96d
rupeshsaklecha-ai <rupeshsaklecha@gmail.com>
2026-07-28 15:32:01 +0530

    Update discover.py

--- discover.py
+++ discover.py
@@ -11,7 +11,7 @@ def get_ent_mapping(sw):
         ENT_ALIAS
     )
 
-print("\nENT_ALIAS WALK")
+    print("\nENT_ALIAS WALK")
     print("-" * 60)
 
     for oid, value in rows:
@@ -20,28 +20,6 @@ print("\nENT_ALIAS WALK")
         print()
 
     return {}
-    
-    mapping = {}
-
-    for oid, value in rows:
-
-        # Value Example:
-        # IF-MIB::ifIndex.30
-        # या
-        # iso.org.dod.internet.mgmt.mib-2.interfaces.ifTable.ifEntry.ifIndex.30
-
-        if "ifIndex." in value:
-
-            try:
-                ifindex = int(value.split(".")[-1])
-                ent = int(oid.split(".")[-1])
-
-                mapping[ifindex] = ent
-
-            except:
-                pass
-
-    return mapping
 
 
 def discover():
@@ -66,29 +44,15 @@ def discover():
         print("SNMP Connected")
         print()
 
-        # Get ifIndex -> entPhysicalIndex Mapping
-        mapping = get_ent_mapping(sw)
+        get_ent_mapping(sw)
 
-        print("Entity Mapping")
-        print("-" * 40)
-
-        if len(mapping) == 0:
-            print("No Mapping Found")
-        else:
-            for ifindex in sorted(mapping):
-                print(f"ifIndex {ifindex} -> entPhysicalIndex {mapping[ifindex]}")
-
-        print()
-        print("Ports")
+        print("\nPorts")
         print("-" * 40)
 
         for oid, value in ports:
 
             if "XGigabitEthernet" in value:
 
-                idx = int(oid.split(".")[-1])
+                idx = oid.split(".")[-1]
 
-                if idx in mapping:
-                    print(f"{value:30} ifIndex={idx} entPhysicalIndex={mapping[idx]}")
-                else:
-                    print(f"{value:30} ifIndex={idx} entPhysicalIndex=Not Found")
+                print(f"{value:30} ifIndex={idx}")
