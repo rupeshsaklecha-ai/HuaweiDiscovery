7f7bfdb99ed8da80a8c47b230bdb3010fb3dbdf6
rupeshsaklecha-ai <rupeshsaklecha@gmail.com>
2026-07-28 16:13:33 +0530

    Update export_excel.py

--- export_excel.py
+++ export_excel.py
@@ -1,1 +1,26 @@
+from openpyxl import Workbook
 
+
+def export_excel(ip, data):
+
+    wb = Workbook()
+
+    ws = wb.active
+
+    ws.title = "Discovery"
+
+    ws.append([
+        "Port Name",
+        "ifIndex",
+        "entPhysicalIndex"
+    ])
+
+    for row in data:
+
+        ws.append(row)
+
+    filename = f"HuaweiDiscovery_{ip}.xlsx"
+
+    wb.save(filename)
+
+    print("Saved :", filename)
