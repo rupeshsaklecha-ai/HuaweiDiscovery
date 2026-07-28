88c9f90ae90a8f27e1353bb72b3a04f4370540f1
rupeshsaklecha-ai <rupeshsaklecha@gmail.com>
2026-07-28 16:49:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from discover import discover
-
 print("=" * 60)
-print("Huawei Discovery Tool V2")
+print("Huawei Discovery Tool V2.0")
+print("Developed by Sun Technologies")
 print("=" * 60)
 
 ip = input("Switch IP Address : ").strip()
+
 community = input("SNMP Community   : ").strip()
 
-discover(ip, community)
+port = input("SNMP Port [161]   : ").strip()
+
+if port == "":
+    port = 161
+else:
+    port = int(port)
+
+discover(ip, community, port)
 
 input("\nPress ENTER to Exit...")
